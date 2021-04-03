ef54a91a08eddfc86ae99c12cdc940cd69af5681
Blake Wilson <blake@blake.id>
2021-04-03 15:06:26 -0500

    Fix ellipsis when not at max line length

---
+++ __tests__/getLinesFromTextTest.ts
@@ -0,0 +1,7 @@
+test('it should fragment a word larger than the max width of the text block', () => {})
+
+test('it should remove lines that go over the region width/height specified', () => {})
+
+test('it should add an ellipsis at the very last line', () => {})
+
+test('it should not remove lines since overflow is turned on', () => {})
