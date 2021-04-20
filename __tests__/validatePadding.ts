ed6743d9b804b6f5462873358afbdb2e2ab587ae
Blake Wilson <blake@blake.id>
2021-04-20 17:50:06 -0500

    Create test case to validate padding positions

---
+++ __tests__/validatePadding.ts
@@ -0,0 +1,31 @@
+import CanvasTextBlock from "../src/CanvasTextBlock";
+
+const canvas = document.createElement("canvas");
+canvas.width = 1000;
+canvas.height = 1000;
+
+test("it should validate the padding config", () => {
+  const xPos = 0;
+  const yPos = 0;
+  const widthHeight = 500;
+  const padding = 50;
+
+  const instance = new CanvasTextBlock(
+    canvas,
+    xPos,
+    yPos,
+    widthHeight,
+    widthHeight,
+    {
+      padding: padding,
+    }
+  );
+
+  expect((instance as any).getTextBlockMaxWidth()).toEqual(
+    widthHeight - padding * 2
+  );
+
+  expect((instance as any).getStartingLineXPos()).toEqual(xPos + padding);
+
+  expect((instance as any).getStartingLineYPos()).toEqual(yPos + padding);
+});
